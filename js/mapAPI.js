2370793f7ec94b5cf2d8a9b802c0e7eb98136a91
unixd34 <113084789+unixd34@users.noreply.github.com>
2023-01-03 15:31:40 +0800

    Add files via upload

--- js/mapAPI.js
+++ js/mapAPI.js
@@ -1,4 +1,6 @@
 function initMap(){
+  var directionsRenderer = new google.maps.DirectionsRenderer();
+  var directionsService = new google.maps.DirectionsService();
     
     // Apply google map api
     var map = new google.maps.Map(document.getElementById("map"), {
@@ -15,27 +17,6 @@ function initMap(){
         },
         streetViewControl: true,
     });
-    // Ormoc city entrance marker
-    var entrance = new google.maps.Marker({
-      position: {lat: 11.1128, lng: 124.5739},
-      map: map
-    });
-    // Ormoc city marker
-    var Ormoc = new google.maps.Marker({
-      position: {lat: 11.038, lng: 124.619},
-      map: map
-    });
-    //Ormoc City entrance navigator
-    var infowindow = new google.maps.InfoWindow({
-      content:"From Tacloban to Ormoc Road (Entrance)"
-    });
-    infowindow.open(map,entrance);
-
-    //Ormoc City navigator
-    var infowindow = new google.maps.InfoWindow({
-      content:"Ormoc City"
-    });
-    infowindow.open(map,Ormoc);
 
     // Search Fast food
     var input = document.getElementById('search');
@@ -48,50 +29,13 @@ function initMap(){
       var markers = [];
       searchBox.addListener('places_changed', function () {
         var places = searchBox.getPlaces();
-        if (places.length == 0)
-          return;
 
-        markers.forEach(function (m) { m.setMap(null); });
-        markers = [];
 
         var bounds = new google.maps.LatLngBounds();
         places.forEach(function(p) {
           if (!p.geometry)
             return;
 
-          var a = p.geometry.location.lat();
-          var b = p.geometry.location.lng();
-          var c = p.name;
-
-          
-
-          var lat = document.getElementById('lat');
-          var lng = document.getElementById('long');
-          var name = document.getElementById('name');
-          lat.innerHTML = a;
-          lng.innerHTML = b;
-          name.innerHTML = c;
-
-          var toLat = a.toString();
-          var toLng = b.toString();
-
-
-          mark = new google.maps.Marker({
-            map: map,
-            title: p.name,
-            position: {lat: a, lng: b},
-            zoom: 15,
-            animation:google.maps.Animation.BOUNCE,
-          });
-          var infowindow = new google.maps.InfoWindow({
-            content: 
-            "Name: " + p.name + 
-            "<br>" + 
-            "Latitude: " + toLat + 
-            "<br>" + 
-            "Longitude: " + toLng
-          });
-          infowindow.open(map,mark);
 
           if (p.geometry.viewport)
             bounds.union(p.geometry.viewport);
@@ -101,4 +45,77 @@ function initMap(){
         
         map.fitBounds(bounds);
       });
+
+
+
+      var input = document.getElementById('to');
+    var searchBox = new google.maps.places.SearchBox(input);
+
+      map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+      });
+
+      var markers = [];
+      searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+        if (places.length == 0)
+          return;
+
+
+        markers.forEach(function (m) { m.setMap(null); });
+        markers = [];
+        var bounds = new google.maps.LatLngBounds();
+
+
+          var a = places.geometry.location.lat();
+          var b = places.geometry.location.lng();
+          var c = places.name;
+
+          var mark = new google.maps.Marker({
+                map: map,
+                title: places.name,
+                position: {lat: a, lng: b},
+                zoom: 15,
+                animation:google.maps.Animation.BOUNCE,
+              });
+
+          infowindow.open(map,mark);
+
+
+          if (p.geometry.viewport) {
+            bounds.union(places.geometry.viewport);
+          }
+          else{
+            bounds.extend(places.geometry.location);
+          }
+
+        
+        map.fitBounds(bounds);
+      });
+
+
+
+      directionsRenderer.setMap(map);
+      document.getElementById("mode").addEventListener("change", () => {
+        var e = document.getElementById("distination");
+        var text = e.options[e.selectedIndex].text;
+
+        const selectedMode = document.getElementById("mode").value;
+
+        directionsService.route({
+            origin: document.getElementById("search").value,
+            destination: text,
+            travelMode: google.maps.TravelMode[selectedMode],
+          })
+          .then((response) => {
+            directionsRenderer.setDirections(response);
+          })
+          .catch((e) => window.alert("There's no type of service like that in this location! " + status));
+      });
 }
+
+
+
+
+
+window.initMap = initMap;
